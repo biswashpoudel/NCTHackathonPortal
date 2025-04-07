7b4970826e06b7ed66f4bfb83f0c73a06fa04fc3
Abishek Pokhrel <abishekpokhrel@Abisheks-MacBook-Air.local>
2025-04-07 12:01:55 -0400

    user modal changes

--- backend/models/user.js
+++ backend/models/user.js
@@ -1,11 +1,14 @@
-// models/User.js
-const mongoose = require('mongoose');
+const mongoose = require("mongoose")
 
-const UserSchema = new mongoose.Schema({
-  username: { type: String, required: true },
+// User Schema
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
-  role: { type: String, enum: ['judge', 'participant', 'mentor', 'admin'], default: 'participant' },
-});
+  role: { type: String, required: true, enum: ["user", "judge", "mentor", "admin"], default: "user" },
+  isParticipating: { type: String, default: "false" }, // can be 'false', 'pending', or 'true'
+  isApproved: { type: Boolean, default: false }, // User approval status
+})
 
-module.exports = mongoose.model('User', UserSchema);
+module.exports = mongoose.model("User", userSchema)
+
